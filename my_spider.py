647701967379734fae3c296795fdbe5b0ce60764
Akhil <144477575+andromeda0004@users.noreply.github.com>
2025-06-18 23:59:29 +0530

    Update my_spider.py

--- my_spider.py
+++ my_spider.py
@@ -1,4 +1,4 @@
-import requests #for the actual get requests which pull the content out of a given page
+import requests #for the actual get requests which pull the content out of a given page.
 from bs4 import BeautifulSoup #used to pull data out of html documents and present it in a way such that navigating the site is easier 
 from urllib.parse import urljoin #for creating absolute links for the spider to naviagte easily
 import time #to use for delays and re-connections
